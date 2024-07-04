0fe8702f41859e41d21821e866561e3e69036625
Luís Miguel <92678690+charmander-01@users.noreply.github.com>
2024-07-03 22:28:26 -0300

    Update Info.fsx

--- Info.fsx
+++ Info.fsx
@@ -1,18 +1,18 @@
-F#
--> Linguagem multiparadigma. Paradigmas suportados: funcional, imperativo e orientado a objetos
--> Open source e cross-plataform
--> Apresenta registros:
-------------------------
+// F#
+// -> Linguagem multiparadigma. Paradigmas suportados: funcional, imperativo e orientado a objetos
+// -> Open source e cross-plataform
+// -> Apresenta registros:
+// ----------/--------------
 // Exemplo de registro:
 type R = 
         { Name : string 
          Age : int }
-------------------------
--> Apresenta uniões descriminadas:
--------------------------------------------------------------------
+// -------------------------
+// -> Apresenta uniões descriminadas:
+// -------------------------------------------------------------------
 // Exemplo de união descriminada
 type Forma =
   | Retangulo of Largura: float * Comprimento: float
   | Circulo of Raio: float
   | Prisma of Largura: float * Altura: float * Profundidade: float
--------------------------------------------------------------------
+// -------------------------------------------------------------------
